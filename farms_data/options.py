94c147e53bc20f5e756aff893aaa3949a6a50fe3
Jonathan Arreguit <jarreguit@gmail.com>
2021-02-24 12:00:11 +0100

    [YAML] Moved YAML methods to dedicated file

--- farms_data/options.py
+++ farms_data/options.py
@@ -1,37 +1,7 @@
 """Options"""
 
 from enum import IntEnum
-import yaml
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
-    import farms_pylog as pylog
-    pylog.warning(
-        'YAML CLoader and CDumper not available'
-        ', switching to Python implementation'
-        '\nThis will run slower than the C alternative'
-    )
-
-
-
-def pyobject2yaml(filename, pyobject, mode='w+'):
-    """Pyobject to yaml"""
-    with open(filename, mode) as yaml_file:
-        yaml.dump(
-            pyobject,
-            yaml_file,
-            default_flow_style=False,
-            sort_keys=False,
-            Dumper=Dumper,
-        )
-
-
-def yaml2pyobject(filename):
-    """Pyobject to yaml"""
-    with open(filename, 'r') as yaml_file:
-        options = yaml.load(yaml_file, Loader=Loader)
-    return options
+from .io.yaml import pyobject2yaml, yaml2pyobject
 
 
 class Options(dict):
