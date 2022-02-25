f5c393b70a95c3eee098031e9c42850853eeb612
Jonathan Arreguit <jarreguit@gmail.com>
2022-02-25 18:04:08 +0100

    [TYPES] Moved types definitions to top package directory

--- farms_data/__init__.py
+++ farms_data/__init__.py
@@ -7,8 +7,8 @@ def get_include_paths():
     """Get include paths"""
     farms_data_path = os.path.dirname(os.path.abspath(__file__))
     directories = [
-        os.path.join(farms_data_path, 'amphibious'),
-        os.path.join(farms_data_path, 'sensors'),
+        os.path.join(farms_data_path, folder)
+        for folder in ['', 'amphibious', 'sensors', 'utils']
     ]
     for directory in directories:
         assert os.path.isdir(directory)
