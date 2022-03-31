a3c738daa0447d9f1ea46c7fdb82434e807fb8fa
Jonathan Arreguit <jarreguit@gmail.com>
2022-04-01 01:09:37 +0200

    [AMPHIBIOUS] Minor refactoring for AmphibiousData

--- farms_data/amphibious/data.py
+++ farms_data/amphibious/data.py
@@ -49,7 +49,7 @@ class JointsControlArray(JointsControlArrayCy):
 
 
 class AmphibiousData(AmphibiousDataCy, AnimatData):
-    """Animat data"""
+    """Amphibious data"""
 
     def __init__(
             self,
@@ -64,51 +64,12 @@ class AmphibiousData(AmphibiousDataCy, AnimatData):
         self.joints = joints
 
     @classmethod
-    def from_file(cls, filename: str):
-        """From file"""
-        pylog.info('Loading data from %s', filename)
-        data = hdf5_to_dict(filename=filename)
-        pylog.info('loaded data from %s', filename)
-        data['n_oscillators'] = len(data['network']['oscillators']['names'])
-        return cls.from_dict(data)
-
-    @classmethod
-    def from_dict(cls, dictionary: Dict):
-        """Load data from dictionary"""
-        n_oscillators = dictionary.pop('n_oscillators')
-        return cls(
-            timestep=dictionary['timestep'],
-            state=OscillatorNetworkState(dictionary['state'], n_oscillators),
-            network=NetworkParameters.from_dict(dictionary['network']),
-            joints=JointsControlArrayCy(dictionary['joints']),
-            sensors=SensorsData.from_dict(dictionary['sensors']),
-        )
-
-    def to_dict(self, iteration: int = None) -> Dict:
-        """Convert data to dictionary"""
-        data_dict = super().to_dict(iteration=iteration)
-        data_dict.update({
-            'state': to_array(self.state.array),
-            'network': self.network.to_dict(iteration),
-            'joints': to_array(self.joints.array),
-        })
-        return data_dict
-
-    def plot(self, times: NDArray[(Any,), float]) -> Dict:
-        """Plot"""
-        plots = {}
-        plots.update(self.state.plot(times))
-        plots.update(self.plot_sensors(times))
-        plots['drives'] = self.network.drives.plot(times)
-        return plots
-
-    @classmethod
     def from_options(
             cls,
             animat_options: AnimatOptions,
             simulation_options: SimulationOptions,
     ):
-        """From options"""
+        """From animat and simulation options"""
 
         # Sensors
         sensors = SensorsData.from_options(
@@ -195,3 +156,42 @@ class AmphibiousData(AmphibiousDataCy, AnimatData):
             network=network,
             joints=JointsControlArray.from_options(animat_options.control),
         )
+
+    @classmethod
+    def from_file(cls, filename: str):
+        """From file"""
+        pylog.info('Loading data from %s', filename)
+        data = hdf5_to_dict(filename=filename)
+        pylog.info('loaded data from %s', filename)
+        data['n_oscillators'] = len(data['network']['oscillators']['names'])
+        return cls.from_dict(data)
+
+    @classmethod
+    def from_dict(cls, dictionary: Dict):
+        """Load data from dictionary"""
+        n_oscillators = dictionary.pop('n_oscillators')
+        return cls(
+            timestep=dictionary['timestep'],
+            state=OscillatorNetworkState(dictionary['state'], n_oscillators),
+            network=NetworkParameters.from_dict(dictionary['network']),
+            joints=JointsControlArrayCy(dictionary['joints']),
+            sensors=SensorsData.from_dict(dictionary['sensors']),
+        )
+
+    def to_dict(self, iteration: int = None) -> Dict:
+        """Convert data to dictionary"""
+        data_dict = super().to_dict(iteration=iteration)
+        data_dict.update({
+            'state': to_array(self.state.array),
+            'network': self.network.to_dict(iteration),
+            'joints': to_array(self.joints.array),
+        })
+        return data_dict
+
+    def plot(self, times: NDArray[(Any,), float]) -> Dict:
+        """Plot"""
+        plots = {}
+        plots.update(self.state.plot(times))
+        plots.update(self.plot_sensors(times))
+        plots['drives'] = self.network.drives.plot(times)
+        return plots
