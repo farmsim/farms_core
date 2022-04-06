c0c0a3ce48e6b7dd1a27e1b18bd8643225d29d51
Jonathan Arreguit <jarreguit@gmail.com>
2022-04-06 17:39:38 +0200

    [SETUP] Updated setup to include *.pxd files

--- setup.py
+++ setup.py
@@ -39,23 +39,23 @@ setup(
     version='0.1',
     author='farmsdev',
     author_email='biorob-farms@groupes.epfl.ch',
-    description='FARMS package for data handling',
+    description='FARMS core package',
     # license='BSD-3',
-    keywords='farms data simulation',
-    # url='',
+    keywords='farms framework animal robot modelling simulation',
+    url='https://gitlab.com/farmsim/farms_core',
     # packages=['farms_core'],
-    packages=find_packages(),
     # long_description=read('README'),
     # classifiers=[
     #     'Development Status :: 3 - Alpha',
     #     'Topic :: Utilities',
     #     'License :: OSI Approved :: BSD License',
     # ],
-    scripts=[],
-    # package_data={'farms_core': [
-    #     'farms_core/templates/*',
-    #     'farms_core/config/*'
-    # ]},
+    packages=find_packages(),
+    package_dir={'farms_core': 'farms_core'},
+    package_data={'farms_core': [
+        f'{folder}*.pxd'
+        for folder in ['', 'array/', 'sensors/', 'model/', 'utils/']
+    ]},
     include_package_data=True,
     include_dirs=[np.get_include(), 'farms_core'],
     ext_modules=cythonize(
