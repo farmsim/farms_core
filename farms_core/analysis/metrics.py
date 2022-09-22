ca7133020b300a8d22d7aeb8f920e08a376db83b
Jonathan Arreguit <jarreguit@gmail.com>
2022-09-22 16:15:16 +0200

    [METRICS] Updated limb swings

--- farms_core/analysis/metrics.py
+++ farms_core/analysis/metrics.py
@@ -4,10 +4,14 @@ import numpy as np
 from ..sensors.sensor_convention import sc
 
 
-def get_limb_swings(contacts_array, feet_indices, threshold=1e-16):
-    """Get limb swing"""
+def get_limb_swings(contacts_array, contact_indices, threshold=1e-16):
+    """Get limb swing (True if in swing, False otherwise)"""
     swings = np.linalg.norm(
-        contacts_array[:, feet_indices, sc.contact_total_x:sc.contact_total_z+1],
+        contacts_array[
+            :,
+            contact_indices,
+            sc.contact_total_x:sc.contact_total_z+1,
+        ],
         axis=-1,
-    ) < threshold  # True if in swing, False otherwise
+    ) < threshold
     return swings
