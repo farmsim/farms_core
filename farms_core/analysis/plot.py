635fc9d94d860e77db87724864751486b7ad2208
Jonathan Arreguit <jarreguit@gmail.com>
2022-09-29 19:59:14 +0200

    [PLOT] Added LaTeX options for Matplotlib

--- farms_core/analysis/plot.py
+++ farms_core/analysis/plot.py
@@ -1,5 +1,6 @@
 """Plotting"""
 
+from cycler import cycler
 import matplotlib.pyplot as plt
 
 
@@ -7,3 +8,16 @@ def grid():
     """Grid"""
     plt.grid(visible=True, which='major', linestyle='--', alpha=0.4)
     plt.grid(visible=True, which='minor', linestyle=':', alpha=0.3)
+
+
+def plt_latex_options():
+    """Latex options for plotting"""
+    plt.rc('axes', prop_cycle=(
+        cycler(linestyle=['-', '--', '-.', ':'])
+        * cycler(color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
+    ))
+    plt.rcParams.update({
+        'text.usetex': True,
+        'font.family': 'serif',
+        'font.serif': ['Palatino'],
+    })
