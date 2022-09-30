635e11b55d5939f86ee865287c9a51de8d3654c9
Jonathan Arreguit <jarreguit@gmail.com>
2022-09-30 21:37:44 +0200

    [ANALYSIS] Separated cycle from LaTeX plotting options

--- farms_core/analysis/plot.py
+++ farms_core/analysis/plot.py
@@ -10,12 +10,16 @@ def grid():
     plt.grid(visible=True, which='minor', linestyle=':', alpha=0.3)
 
 
-def plt_latex_options():
-    """Latex options for plotting"""
+def plt_cycle_options():
+    """Cycle options for plotting"""
     plt.rc('axes', prop_cycle=(
         cycler(linestyle=['-', '--', '-.', ':'])
         * cycler(color=plt.rcParams['axes.prop_cycle'].by_key()['color'])
     ))
+
+
+def plt_latex_options():
+    """Latex options for plotting"""
     plt.rcParams.update({
         'text.usetex': True,
         'font.family': 'serif',
