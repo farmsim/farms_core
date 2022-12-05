4b74667f3e96f5ad0e61a1c97615a1f9d14f9b7b
Jonathan Arreguit <jarreguit@gmail.com>
2022-12-05 09:50:16 +0100

    [PLOT] Added function for plotting matrices

--- farms_core/analysis/plot.py
+++ farms_core/analysis/plot.py
@@ -1,8 +1,11 @@
 """Plotting"""
 
 import os
+from enum import Enum
+from typing import List
 
 import numpy as np
+from nptyping import NDArray, Shape, Float
 from cycler import cycler
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -169,6 +172,109 @@ def plot2d(results, labels, n_data=300, log=False, cmap='cividis', **kwargs):
     cbar = plt.colorbar(imgplot, cax=cax)
     cbar.set_label(labels[2])
     assert not kwargs, kwargs
+
+
+class MatrixLineType(Enum):
+    """Matrix line type"""
+    ROW = 0
+    COLUMN = 1
+
+
+class MatrixLine:
+    """Matrix line"""
+
+    def __init__(self, line_type, index, **kwargs):
+        super().__init__()
+        self.line_type: MatrixLineType = line_type
+        self.index: int = index
+        self.kwargs = kwargs
+
+    @classmethod
+    def row(cls, *args, **kwargs):
+        """Matrix row line"""
+        return cls(line_type=MatrixLineType.ROW, *args, **kwargs)
+
+    @classmethod
+    def column(cls, *args, **kwargs):
+        """Matrix column line"""
+        return cls(line_type=MatrixLineType.COLUMN, *args, **kwargs)
+
+
+def plot_matrix(
+        matrix: NDArray[Shape['Any, Any'], Float],
+        fig_name: str,
+        labels: List[List[str]],
+        clabel: str,
+        **kwargs,
+):
+    """Plot matrix"""
+    lines = kwargs.pop('lines', [])
+    xlabel = kwargs.pop('xlabel', '')
+    ylabel = kwargs.pop('ylabel', '')
+    reduce_x = kwargs.pop('line_y', [])
+    reduce_y = kwargs.pop('line_x', [])
+    reduce_x = kwargs.pop('reduce_x', False)
+    reduce_y = kwargs.pop('reduce_y', False)
+    if reduce_x or reduce_y:
+        labels = labels[:]
+        shape = np.shape(matrix)
+        matrix_nans = np.isnan(matrix)
+        if reduce_y:
+            rows = [
+                not all(matrix_nans[i, :])
+                for i in range(shape[0])
+            ]
+            matrix = matrix[rows, :]
+            labels[0] = np.array(labels[0])[rows]
+        if reduce_x:
+            cols = [
+                not all(matrix_nans[:, i])
+                for i in range(shape[1])
+            ]
+            matrix = matrix[:, cols]
+            labels[1] = np.array(labels[1])[cols]
+    shape = np.shape(matrix)
+    figure = plt.figure(fig_name, figsize=(0.15*shape[1]+2, 0.15*shape[0]))
+    axis = plt.gca()
+    if 0 in shape:
+        return figure
+    assert shape[0] == len(labels[0])
+    assert shape[1] == len(labels[1])
+
+    # Plot matrix
+    cax = axis.matshow(matrix, **kwargs)
+    axis.set_xticks(range(shape[1]))
+    axis.set_yticks(range(shape[0]))
+    axis.set_xticklabels(labels[1], rotation='vertical')
+    axis.set_yticklabels(labels[0], rotation='horizontal')
+
+    # Labels
+    if xlabel:
+        axis.set_xlabel(xlabel)
+        axis.xaxis.set_label_position('top')
+    if ylabel:
+        axis.set_ylabel(ylabel)
+
+    # Plot lines
+    axis.autoscale(False)
+    for line in lines:
+        assert isinstance(line, MatrixLine), f'{line=} if not {MatrixLine}'
+        assert isinstance(line.line_type, MatrixLineType), (
+            f'{line.line_type=} if not {MatrixLineType}'
+        )
+        if line.line_type == MatrixLineType.ROW:
+            axis.plot([-0.5, shape[1]], [line.index, line.index], **line.kwargs)
+        else:
+            axis.plot([line.index, line.index], [-0.5, shape[0]], **line.kwargs)
+
+    # Colorbar
+    cbar = figure.colorbar(cax)
+    cbar.set_label(clabel)
+    # divider = make_axes_locatable(axis)
+    # dax = divider.append_axes('right', size='5%', pad=0.05)
+    # cbar = plt.colorbar(cax, cax=dax)
+    # cbar.set_label(clabel)
+    return figure
 
 
 def colorgraph(
