df85604c69019b336064d33bf3e6102d29d24147
Shravan Tata <shravantr@gmail.com>
2025-07-18 12:37:45 -0500

    [MAIN] Moved package-data from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -29,28 +29,6 @@ Options.closure_freelist_size = 8
 
 
 setup(
-    name='farms_core',
-    version='0.1',
-    author='farmsdev',
-    author_email='biorob-farms@groupes.epfl.ch',
-    description='FARMS core package',
-    # license='BSD-3',
-    keywords='farms framework animal robot modelling simulation',
-    url='https://gitlab.com/farmsim/farms_core',
-    # packages=['farms_core'],
-    # long_description=read('README'),
-    # classifiers=[
-    #     'Development Status :: 3 - Alpha',
-    #     'Topic :: Utilities',
-    #     'License :: OSI Approved :: BSD License',
-    # ],
-    packages=find_packages(),
-    package_dir={'farms_core': 'farms_core'},
-    package_data={'farms_core': [
-        f'{folder}*.pxd'
-        for folder in ['', 'array/', 'sensors/', 'model/', 'utils/']
-    ]},
-    include_package_data=True,
     include_dirs=[np.get_include(), 'farms_core'],
     ext_modules=cythonize(
         [
