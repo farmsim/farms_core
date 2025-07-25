45d0d19ca09acd5502a8993d6b13889dbbdd2dd3
Jonathan Arreguit <jarreguit@gmail.com>
2025-07-26 00:48:10 +0200

    [DOC] Added module for writing and generating docs

---
+++ farms_core/doc.py
@@ -0,0 +1,35 @@
+"""Documentation"""
+
+from typing import List
+
+
+class ClassDoc:
+    """Class documentation"""
+
+    def __init__(self, name, description, class_type, children):
+        super().__init__()
+        self.name: str = name
+        self.class_type = class_type
+        self.description: str = description
+        self.children: List[ClassDoc] = children
+
+
+class ChildDoc:
+    """Child documentation"""
+
+    def __init__(self, name, description, class_type, class_link=None):
+        super().__init__()
+        self.name: str = name
+        self.class_type = class_type
+        self.class_link = class_type if class_link is None else class_link
+        self.description: str = description
+
+
+def get_inherited_doc_children(cls) -> List[ChildDoc]:
+    """Get inherited doc children"""
+    return [
+        child
+        for base in cls.__bases__
+        if hasattr(base, 'doc')
+        for child in base.doc().children
+    ]
