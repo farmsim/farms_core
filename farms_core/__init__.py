d5eb81bb82bd4b2d5f326456e92662568160777b
Jonathan Arreguit <jarreguit@gmail.com>
2025-08-05 17:49:54 +0200

    [TOML] Preliminary solution for TOML installation

--- farms_core/__init__.py
+++ farms_core/__init__.py
@@ -3,12 +3,12 @@
 import os
 
 
-def get_include_paths():
+def get_include_paths() -> list[str]:
     """Get include paths"""
     farms_core_path = os.path.dirname(os.path.abspath(__file__))
     directories = [
         os.path.join(farms_core_path, folder)
-        for folder in ['', 'array', 'sensors', 'model', 'utils']
+        for folder in ['..', '', 'array', 'sensors', 'model', 'utils']
     ]
     for directory in directories:
         assert os.path.isdir(directory)
