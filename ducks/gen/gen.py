f9f30c2197046109bfe08122a4786cb5de6e7fb8
Jonathan Arreguit <jarreguit@gmail.com>
2025-08-27 01:14:16 +0200

    [DOCS] Doc generation function for other packages

--- ducks/gen/gen.py
+++ ducks/gen/gen.py
@@ -1,4 +1,4 @@
-"""Data documentation"""
+"""Automatic documentation"""
 
 import os
 import inspect
@@ -8,13 +8,12 @@ import numpy as np
 from tabulate import tabulate
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from farms_core import pylog
+from farms_core.doc import ClassDoc, doc_class_markdown
 from farms_core.sensors.sensor_convention import sc
 from farms_core.experiment.options import ExperimentOptions
 from farms_core.experiment.data import ExperimentData
 from farms_core.sensors.data import (
     SensorsData,
-    ClassDoc,
     LinkSensorArray,
     JointSensorArray,
     ContactsArray,
@@ -128,97 +127,13 @@ def sensors():
         output.write(text)
 
 
-def config_doc(doc, level, classes_defined):
-    """Config documentation"""
-    class_name = (
-        doc.class_type
-        if isinstance(doc.class_type, str)
-        else doc.class_type.__name__
-    )
-    class_ref = f"<a id='ref-{class_name}'></a>"
-    text = f"{class_ref}\n{level} {class_name}\n\n{doc.description}\n"
-
-    # Children short descriptions
-    text += "\n"
-    for child in doc.children:
-        module = inspect.getmodule(child.class_link)
-        class_link_name = (
-            child.class_link.__name__
-            if not isinstance(child.class_link, str)
-            else child.class_link
-        )
-        reference = (
-            f"`{class_link_name}`"
-            if module is None
-            or "farms_" not in module.__name__
-            else child.class_type.replace(
-                class_link_name,
-                f"[{class_link_name}](#ref-{class_link_name})",
-            )
-            if isinstance(child.class_type, str)
-            else f"[{class_link_name}](#ref-{class_link_name})"
-        )
-        text += f"- `{child.name}` ({reference}):"
-        text += f" {child.description}\n"
-    text += "\n"
-
-    # Children recursive descriptions
-    for child in doc.children:
-        if hasattr(child.class_link, "doc"):
-            if child.class_link.__name__ not in classes_defined:
-                text += config_doc(
-                    doc=child.class_link.doc(),
-                    level=f"#{level}",
-                    classes_defined=classes_defined,
-                )
-                classes_defined.append(child.class_link.__name__)
-        else:
-            module = inspect.getmodule(child.class_link)
-            if module is not None and "farms_" in module.__name__:
-                pylog.warning("WARNING: %s does not have doc", child.class_link)
-
-    # Whitespace
-    for _ in range(5):
-        text = text.replace("\n\n\n", "\n\n")
-
-    text += "\n"
-    return text
-
-
-def data():
+def doc_to_readme(class_to_document, output_path):
     """Main"""
-
-    path_md = os.path.expandvars("$FARMS_SRC/farms_core/farms_core/experiment/README.md")
-    classes_defined = []
-    text = config_doc(
-        doc=ExperimentData.doc(),
+    path_md = os.path.expandvars(output_path)
+    text = doc_class_markdown(
+        doc=class_to_document.doc(),
         level="#",
-        classes_defined=classes_defined,
     )
-
-    # Whitespace
-    for _ in range(5):
-        text = text.replace("\n\n\n", "\n\n")
-
-    print(text)
-    with open(path_md, "w+", encoding="utf-8") as output:
-        output.write(text)
-
-def model():
-    """Main"""
-
-    path_md = os.path.expandvars("$FARMS_SRC/farms_core/farms_core/README.md")
-    classes_defined = []
-    text = config_doc(
-        doc=ExperimentOptions.doc(),
-        level="#",
-        classes_defined=classes_defined,
-    )
-
-    # Whitespace
-    for _ in range(5):
-        text = text.replace("\n\n\n", "\n\n")
-
     print(text)
     with open(path_md, "w+", encoding="utf-8") as output:
         output.write(text)
@@ -228,9 +143,15 @@ def main():
     """Main"""
     sensors()
     print("\n")
-    data()
+    doc_to_readme(
+        class_to_document=ExperimentData,
+        output_path="$FARMS_SRC/farms_core/farms_core/experiment/README.md",
+    )
     print("\n")
-    model()
+    doc_to_readme(
+        class_to_document=ExperimentOptions,
+        output_path="$FARMS_SRC/farms_core/farms_core/README.md",
+    )
 
 
 if __name__ == "__main__":
--- farms_core/doc.py
+++ farms_core/doc.py
@@ -1,7 +1,7 @@
 """Documentation"""
 
-
-from typing import Callable
+import inspect
+from . import pylog
 
 
 class ClassDoc:
@@ -35,3 +35,68 @@ def get_inherited_doc_children(cls) -> list[ChildDoc]:
         if hasattr(base, 'doc')
         for child in base.doc().children
     ]
+
+
+def doc_class_markdown(doc, level, classes_defined=None, clean_whitespace=True):
+    """Config documentation"""
+
+    # Avoid class documentation repetitions
+    if classes_defined is None:
+        classes_defined = []
+
+    # Class to document
+    class_name = (
+        doc.class_type
+        if isinstance(doc.class_type, str)
+        else doc.class_type.__name__
+    )
+    class_ref = f"<a id='ref-{class_name}'></a>"
+    text = f"{class_ref}\n{level} {class_name}\n\n{doc.description}\n"
+
+    # Children short descriptions
+    text += "\n"
+    for child in doc.children:
+        module = inspect.getmodule(child.class_link)
+        class_link_name = (
+            child.class_link.__name__
+            if not isinstance(child.class_link, str)
+            else child.class_link
+        )
+        reference = (
+            f"`{class_link_name}`"
+            if module is None
+            or "farms_" not in module.__name__
+            else child.class_type.replace(
+                class_link_name,
+                f"[{class_link_name}](#ref-{class_link_name})",
+            )
+            if isinstance(child.class_type, str)
+            else f"[{class_link_name}](#ref-{class_link_name})"
+        )
+        text += f"- `{child.name}` ({reference}):"
+        text += f" {child.description}\n"
+    text += "\n"
+
+    # Children recursive descriptions
+    for child in doc.children:
+        if hasattr(child.class_link, "doc"):
+            if child.class_link.__name__ not in classes_defined:
+                text += doc_class_markdown(
+                    doc=child.class_link.doc(),
+                    level=f"#{level}",
+                    classes_defined=classes_defined,
+                )
+                classes_defined.append(child.class_link.__name__)
+        else:
+            module = inspect.getmodule(child.class_link)
+            if module is not None and "farms_" in module.__name__:
+                pylog.warning("WARNING: %s does not have doc", child.class_link)
+
+    text += "\n"
+
+    # Whitespace
+    if clean_whitespace:
+        while "\n\n\n" in text:
+            text = text.replace("\n\n\n", "\n\n")
+
+    return text
