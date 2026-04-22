e42b32c24855fc6d1da350c832b15d3aaae64785
Shravan Tata <shravantr@gmail.com>
2026-04-22 19:37:29 -0400

    [INIT] Improved get_include_paths and added __version__
    Removed the explicit ".." in the paths and replaced it with
    os.path.dirname(farms_core_path) instead for a cleaner and reliable interface.

--- farms_core/__init__.py
+++ farms_core/__init__.py
@@ -3,13 +3,19 @@
 import os
 
 
+# Main version
+__version__ = "0.2.0"
+
+
 def get_include_paths() -> list[str]:
     """Get include paths"""
     farms_core_path = os.path.dirname(os.path.abspath(__file__))
+    parent_path = os.path.dirname(farms_core_path)
     directories = [
         os.path.join(farms_core_path, folder)
-        for folder in ['..', '', 'array', 'sensors', 'model', 'utils']
+        for folder in ['', 'array', 'sensors', 'model', 'utils']
     ]
+    directories.insert(0, parent_path)
     for directory in directories:
         assert os.path.isdir(directory)
     return directories
